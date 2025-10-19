59c15ce4a07f5ff76499b39f4402ce542e908c71
Dr. Pratap Senecha <dr.pratapseerwi@gmail.com>
2025-10-19 13:23:18 +0530

    Use python (not python3) in start.sh for Railway

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ python manage.py migrate --noinput
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Start Gunicorn using the correct port from Railway
+# Start Gunicorn server (use python, not python3)
 exec gunicorn ShradhaHMS.wsgi:application --bind 0.0.0.0:$PORT
